10463bbb47811a0ffa37ca8902fb6260cb0ac870
Christo77793 <christo77793@gmail.com>
2021-04-25 22:44:13 +0530

    Added working implementation

--- ChatBox.py
+++ ChatBox.py
@@ -5,7 +5,7 @@ from PyQt5 import QtWidgets as Qtw
 from PyQt5 import QtGui as Qtg
 from PyQt5 import QtCore as Qtc
 
-#code functions
+# code functions
 from langdetect import detect
 
 # Code to show error, if the app crashes
@@ -32,18 +32,22 @@ class ChatBox(Qtw.QMainWindow):
 
         self.show()  # show the UI
 
-        self.human_text = ""
+        # Sequence variables
+
+        # chat_bot_sequence will show contain the sequences that program will go through
+        self.chat_bot_sequence = ["text_or_speech", "detect_language", ""]
+
+        # we use current sequence to update to the next sequence
+        self.current_sequence = self.chat_bot_sequence[0]
 
         self.send_message(False, "Hey I am Kratzz!")
-
         self.send_message(False, "I am your personal translator.")
 
         self.choice_of_input()
 
-        self.pushButton_3.clicked.connect(lambda: self.send_message(True))
+        self.sendButton.clicked.connect(lambda: self.send_message(True, message=self.userInputText.toPlainText()))
 
         self.minimiseButton.clicked.connect(lambda: self.showMinimized())
-
         self.closeButton.clicked.connect(lambda: self.close())
 
     def choice_of_input(self):
@@ -51,60 +55,73 @@ class ChatBox(Qtw.QMainWindow):
         self.send_message(False, '1. Text')
         self.send_message(False, '2. Speech')
 
-        # human_text = input()
-        # human_text = human_text.lower()
-        # print(human_text)
-
-
     def send_message(self, message_from, message=""):
 
-        text = self.userInputText.toPlainText()
+        correct_input = True  # if invalid input program does not go further
 
-        if text == "" and message_from:
+        if message == "" and message_from:
             pass
+
         else:
 
-            temp_dataset = {1: message_from, 2: text}
-
+            temp_dataset = {1: message_from, 2: message}
             self.messages.append(temp_dataset)
 
-            if temp_dataset[1]: 
+            if temp_dataset[1]:
                 # User
                 self.text_display += f"<div style='color: #ffa500'>[You]: {temp_dataset[2]}</div> <br />"
+
                 self.chatsText.setText(self.text_display)
 
-                self.human_text = self.userInputText.toPlainText()
+                if self.current_sequence == "text_or_speech":
 
-                print("Human Text Value is ", self.human_text)
+                    human_text = self.userInputText.toPlainText()
 
-                if self.human_text == "text":
-                    # run code
-                    self.selected_text()
+                    if human_text.lower() == "text":  # converting to lower case
+                        self.selected_text()
 
-                elif self.human_text == "speech":
-                    # run code
-                    pass
+                    elif human_text.lower() == "speech":  # converting to lower case
+                        # we can define fn to what should happen if user selects speech
+                        pass
+
+                    else:
+                        # will update the correct_input var so that the program will continue only if there is a valid input
+                        correct_input = False
+                        self.send_message(False, "Please enter either Text or Speech alone.")
+
+                if self.current_sequence == "detect_language":
+                    language_to_detect = self.userInputText.toPlainText()
+                    self.detect_language(language_to_detect)
+
+                current_sequence_location = int(self.chat_bot_sequence.index(f"{self.current_sequence}"))
+
+                print(correct_input)
+
+                if correct_input:  # only update to go to the next sequence if all inputs are valid
+                    if (current_sequence_location + 1) < len(self.chat_bot_sequence):
+                        self.current_sequence = self.chat_bot_sequence[current_sequence_location + 1]
 
             elif not temp_dataset[1]:
                 # Bot
                 self.text_display += f"<div style='color: #acc9e6'>[Bot]: {message}</div> <br />"
                 self.chatsText.setText(self.text_display)
 
-            # self.userInputText.setText("") 
+            self.userInputText.setText("")
 
     def selected_text(self):
-        # detect_language = input("PLease enter the text which you wish to translate: ")
+        self.send_message(False, "Please enter the text which you wish to translate: ")
 
-        self.send_message(False, "PLease enter the text which you wish to translate: ")
+    def detect_language(self, detect_language):
+        self.send_message(False, f"User's input lang is: {detect(detect_language)}")
 
-        
-        self.detect_language_var = self.userInputText.toPlainText()
+        self.ask_user_about_lang_pref()  # calling kranti fn
 
-    
-    # def detect_language(self):
-
-
-
+    def ask_user_about_lang_pref(self):
+        self.send_message(False, "In which language do you wish to translate?")
+        self.send_message(False, "1. English")
+        self.send_message(False, "2. Hindi")
+        self.send_message(False, "3. Deutsch")
+        self.send_message(False, "4. French")
 
     def move_with_click_title_bar(self, event):
 
--- maincode.py
+++ maincode.py
@@ -4,9 +4,10 @@ import speech_recognition as sr
 from transformers import pipeline
 import requests
 
-#pip install langdetect, google_trans_new, speechrecognition, requests, transformers torch senterpiece.
+# pip install langdetect, google_trans_new, speechrecognition, requests, transformers torch senterpiece.
 
 t = google_translator()
+
 
 # this function will take users choice on input text or speech
 def choice_of_input():
@@ -16,6 +17,7 @@ def choice_of_input():
     choice_of_input.human_text = input()
     choice_of_input.human_text = choice_of_input.human_text.lower()
     print(choice_of_input.human_text)
+
 
 # this function will take user's language prefernce and convert it into lower
 def ask_user_about_lang_pref():
@@ -27,7 +29,8 @@ def ask_user_about_lang_pref():
     ask_user_about_lang_pref.pref = input("Enter: ")
     ask_user_about_lang_pref.pref = ask_user_about_lang_pref.pref.lower()
 
-#this function will ask whether the user wants to proceed further or not
+
+# this function will ask whether the user wants to proceed further or not
 def to_continue_further():
     print("Do you wish to convert it into any other language?")
     print("Y/N")
@@ -41,7 +44,8 @@ def to_continue_further():
     else:
         print("Sorry, I can't understand")
 
-#this function converts audio into text by using speech recognizing library
+
+# this function converts audio into text by using speech recognizing library
 def convert_audio_to_text_detect():
     r1 = sr.Recognizer()
     with sr.Microphone() as source:
@@ -56,9 +60,9 @@ def convert_audio_to_text_detect():
         except sr.RequestError as e:
             print('failed'.format(e))
 
-#extra feature added to analyse the sentiments of the text (to tell whether the sentence is negative or positive)
-def analyse_sentiments(detected_language):
 
+# extra feature added to analyse the sentiments of the text (to tell whether the sentence is negative or positive)
+def analyse_sentiments(detected_language):
     detect_language = detected_language
 
     print("Do you want the sentiment analysis of this sentenc?")
@@ -67,8 +71,8 @@ def analyse_sentiments(detected_language):
     y_n = y_n.lower()
     if y_n == 'y':
         my_model = pipeline('sentiment-analysis')
-        print(my_model(detect_language)) #text to be entered
-        #how to use the detect_language variable in this function????
+        print(my_model(detect_language))  # text to be entered
+        # how to use the detect_language variable in this function????
         #
     elif y_n == 'n':
         print("Thank you! ")
@@ -76,13 +80,14 @@ def analyse_sentiments(detected_language):
     else:
         print("Sorry, I can't understand")
 
+
 # ================================================MAIN FUNCTION =======================================================
 
 def main_function():
     choice_of_input()
     if choice_of_input.human_text == 'text':
 
-        detect_language = input("PLease enter the text which you wish to translate: ")
+        detect_language = input("Please enter the text which you wish to translate: ")
 
         print("Language: ", detect(detect_language))
         ask_user_about_lang_pref()
@@ -90,52 +95,56 @@ def main_function():
         if ask_user_about_lang_pref.pref == 'english':
 
             print(t.translate(detect_language, lang_tgt='en'))
-            
-            # print("Detect Language Value: ", detect_language)
-            # print("Detect Language Value: ", type(detect_language))
-            
-            analyse_sentiments(detect_language) #ADDED FOR TRIAL PURPOSES ONLY
-            
+            analyse_sentiments(detect_language)  # ADDED FOR TRIAL PURPOSES ONLY
+
             # to_continue_further()
         elif ask_user_about_lang_pref.pref == 'hindi':
 
             print(t.translate(detect_language, lang_tgt='hi'))
-            
             to_continue_further()
 
         elif ask_user_about_lang_pref.pref == 'deutsch':
+
             print(t.translate(detect_language, lang_tgt='de'))
             to_continue_further()
 
         elif ask_user_about_lang_pref.pref == 'french':
+
             print(t.translate(detect_language, lang_tgt='fr'))
             to_continue_further()
 
         else:
+
             print('Try Again!')
 
     elif choice_of_input.human_text == 'speech':
+
         convert_audio_to_text_detect()
         ask_user_about_lang_pref()
+
         if ask_user_about_lang_pref.pref == 'english':
             print(t.translate(convert_audio_to_text_detect.get, lang_tgt='en'))
             to_continue_further()
+
         elif ask_user_about_lang_pref.pref == 'hindi':
             print(t.translate(convert_audio_to_text_detect.get, lang_tgt='hi'))
             to_continue_further()
+
         elif ask_user_about_lang_pref.pref == 'deutsch':
             print(t.translate(convert_audio_to_text_detect.get, lang_tgt='de'))
             to_continue_further()
+
         elif ask_user_about_lang_pref.pref == 'french':
             print(t.translate(convert_audio_to_text_detect.get, lang_tgt='fr'))
             to_continue_further()
+
         else:
             print('Try Again!')
+
     else:
         print("I am afraid, I can't help you!")
+
 
 print('Hey I am Kratzz.')
 print('I am your personal translator.')
 main_function()
-
-
